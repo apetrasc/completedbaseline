1452d469d80fdf3aa358159709c388e0dbfa6009
apetrasc <apetrasc@gmail.com>
2024-10-04 14:30:43 +0200

    some progress

--- 01_Ret180/01_velocity_yp15/sandbox/ViT_tf.py
+++ 01_Ret180/01_velocity_yp15/sandbox/ViT_tf.py
@@ -6,16 +6,15 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import optimizers
 from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from tensorflow.keras.datasets import cifar10
-#from vit_keras import vit, utils
+from vit_keras import vit, utils
 # from ../src.utils import 
 import sys
 sys.path.append('/mimer/NOBACKUP/groups/kthmech/sadanori/01_Ret180/01_velocity_yp15')
 #from src.vit import vit_b16 
-#import tensorflow_addons as tfa
-from vit_keras import vit, utils
+import tensorflow_addons as tfa
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
+import numpy as np
 import matplotlib.pyplot as plt
 import gc
 
@@ -44,3 +43,57 @@ def build_model():
 
     model_final = Model(inputs=inputs, outputs=outputs)
     return model_final
+seed = 2022
+np.random.seed(seed)
+tf.random.set_seed(seed)
+(train_data, train_label), (test_data, test_label) = cifar10.load_data()
+train_label = to_categorical(train_label)
+test_label = to_categorical(test_label)
+train_data = (train_data/255.).astype("float32")
+test_data = (test_data/255.).astype("float32")
+batch_size = 32
+X_train, X_valid, y_train, y_valid = train_test_split(train_data, train_label, random_state=seed, shuffle=True)
+datagen = ImageDataGenerator(rotation_range=15, width_shift_range=0.2, zoom_range=0.2, horizontal_flip=True)
+train_generator = datagen.flow(X_train, y_train, batch_size=batch_size)
+model = build_model()
+model.compile(optimizer=optimizers.SGD(learning_rate=0.01, momentum=0.9), loss="categorical_crossentropy", metrics=["accuracy"])
+model.summary()
+print("\n")
+model.fit(train_generator,
+          steps_per_epoch=200,
+          epochs=2,
+          validation_data=(X_valid, y_valid),
+         )
+gc.collect()
+#Set training callbacks
+plateau = ReduceLROnPlateau(monitor="val_loss", factor=0.7, patience=1, verbose=1)
+earlystopping = EarlyStopping(monitor="val_loss", patience=3, verbose=1)
+
+#Switch ViT layer to trainable for fine tuning
+for layer in model.layers:
+    layer.trainable = True
+    
+#Requires compile again to activate trainable=True
+model.compile(optimizer=optimizers.SGD(learning_rate=0.001, momentum=0.9), loss="categorical_crossentropy", metrics=["accuracy"])
+model.summary()
+print("\n")
+history = model.fit(train_generator,
+                    steps_per_epoch=200, #you can delete this parameter to achieve more accuracy, but takes much time.
+                    epochs=10,
+                    validation_data=(X_valid, y_valid),
+                    callbacks=[plateau, earlystopping]
+                   )
+print("\nTest Accuracy: ", accuracy_score(np.argmax(test_label, axis=1), np.argmax(model.predict(test_data), axis=1)))
+
+fig, ax = plt.subplots(figsize=(12, 4), nrows=1, ncols=2)
+ax[0].plot(history.history["loss"], c="r", label="train loss")
+ax[0].plot(history.history["val_loss"], c="b", label="val loss")
+ax[0].set_xlabel("Epoch")
+ax[0].set_ylabel("Loss")
+ax[0].legend()
+
+ax[1].plot(history.history["accuracy"], c="r", label="train accuracy")
+ax[1].plot(history.history["val_accuracy"], c="b", label="val accuracy")
+ax[1].set_xlabel("Epoch")
+ax[1].set_ylabel("Accuracy")
+ax[1].legend()
